83b4877d0e2ff05ecd4acdb6eac66503085a93aa
theogiraudet <theo.giraudet@orange.fr>
2025-02-28 17:35:21 +0100

    📝 docs: add "mentions legales" page (#338)

--- docs/conf.py
+++ docs/conf.py
@@ -79,6 +79,8 @@ html_theme_options = {
     "navbar_persistent": ["search-button"],
     "navigation_with_keys": True,
     "use_edit_page_button": True,
+    "footer_center": ["mention-legales-footer.html"],
+    # "extra_footer": '<a href="/mentions-legales">Mentions Légales</a>',
     "header_links_before_dropdown": 3,
     "logo": {
         "text": "Bookshelf",
